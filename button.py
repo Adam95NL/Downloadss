a6e34ad922ba39d61914a32f24e94d46ba0d1ed4
Adam95NL <adamnoorariss@gmail.com>
2022-09-22 15:15:23 +0200

    oefeningen

--- button.py
+++ button.py
@@ -4,13 +4,23 @@ from gpiozero import Button #importeer LED van de libary gpiozero
 
 
 varLedg = LED(3) #varLed is aangesloten op GPIO18
-button = Button(1, pull_up = False)
+button = Button(1, pull_up = False) #button is aangesloten op pin 1 en is een pull_down weerstand
+variable = 0 #variable is gelijk aan 0
+lastbuttonstate = 0# last buttonstate is gelijk aan 0
 
 while True: #zolang de status true is dan
-    if button.value == True:
-        varLedg.on() #stuur het ledje aan
-    else:
-        varLedg.off() #stuur het ledje aan
+    if button.value != lastbuttonstate:# als de button.value niet gelijk is aan de vorige buttonstate is dan
+        if button.value == True:# als de button.value hoog is dan 
+            if variable == 1:#als de variable gelijk is aan 1 dan
+                varLedg.off() #stuur het ledje uit  
+                variable = 0 #maak variable gelijk aan 0
+                sleep(0.2) #wacht 0.2 seconden
+            else: # anders 
+                varLedg.on() #stuur het ledje aan      
+                variable = 1 #maak variable gelijk aan 1
+                sleep(0.2) #wacht 0.2 seconden
+        lastbuttonstate = button.value #maak de vorige buttonstate gelijk aan de button.value
+
     
     
         
